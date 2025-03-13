d6ca575c7604189963791c464bdd4578dbf8f84c
ablakt <88093215+ablakt@users.noreply.github.com>
2025-03-13 20:52:19 +0900

    redis started

--- src/main/java/com/qweuio/chat/persistence/MessagePersistingService.java
+++ src/main/java/com/qweuio/chat/persistence/MessagePersistingService.java
@@ -6,10 +6,13 @@ import com.qweuio.chat.websocket.dto.ProcessedMessageDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class MessagePersistingService {
@@ -17,10 +20,13 @@ public class MessagePersistingService {
   final String sendMsgTopic = "${chatapp.kafka.message-topic}";
   @Autowired
   ChatMessageRepository messageRepository;
+  @Autowired
+  RedisTemplate<String, List<ProcessedMessageDTO>> chatMessageCacheTemplate;
 
   @KafkaListener(id = "messagePersister", topics = sendMsgTopic)
   public void persistMessage(ProcessedMessageDTO message) {
     logger.info("persisting message sent from {} to chatroom {} at {}", message.senderId(), message.chatroomId(), Instant.now());
+    chatMessageCacheTemplate.opsForList().leftPush("chatroom-" + message.chatroomId(), Collections.singletonList(message));
     messageRepository.save(new ChatMessage(null, message.senderId(), message.chatroomId(), Instant.now(), message.message()));
   }
 }
---
+++ src/main/java/com/qweuio/chat/persistence/RedisConfig.java
@@ -0,0 +1,22 @@
+package com.qweuio.chat.persistence;
+
+import com.fasterxml.jackson.databind.ser.Serializers;
+import com.qweuio.chat.websocket.dto.ProcessedMessageDTO;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+import java.util.List;
+
+@Configuration
+public class RedisConfig {
+  @Bean
+  public RedisTemplate<String, List<ProcessedMessageDTO>> redisTemplate(RedisConnectionFactory connectionFactory) {
+    RedisTemplate<String, List<ProcessedMessageDTO>> template = new RedisTemplate<>();
+    template.setConnectionFactory(connectionFactory);
+    template.setDefaultSerializer(RedisSerializer.json());
+    return template;
+  }
+}
