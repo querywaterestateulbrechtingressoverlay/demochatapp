40edb4474a1f6edef672bd0f0cfe8db10c3dd257
qwerty <abc@def.ghi>
2025-07-31 21:45:34 +0900

    checkp

--- start-kubernetes.sh
+++ start-kubernetes.sh
@@ -1,4 +1,7 @@
 cd chat-app && rm -rf build/ && ./gradlew bootJar && cd ..
+docker build -t localhost:5000/frontend frontend
 docker build -t localhost:5000/chat-app chat-app
+docker push localhost:5000/frontend
 docker push localhost:5000/chat-app
+docker image remove localhost:5000/frontend
 docker image remove localhost:5000/chat-app
